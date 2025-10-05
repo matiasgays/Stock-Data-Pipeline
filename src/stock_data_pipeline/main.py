26d5fc40ae83e7e68518edbd4fe57c9c5eaaa7e0
MELLO <ing.matiasgays@gmail.com>
2025-10-05 21:42:56 +0000

    add pylint

--- src/stock_data_pipeline/main.py
+++ src/stock_data_pipeline/main.py
@@ -1,9 +1,14 @@
+"""Main module for the stock data pipeline project."""
+
 from rich.console import Console
 
 console = Console()
 
+
 def main():
-    console.print("[bold green]Hello from uv project")        
+    """Print a colored greeting to the terminal."""
+    console.print("[bold green]Hello from uv project!")
+
 
 if __name__ == "__main__":
     main()
